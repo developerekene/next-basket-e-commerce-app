abaeba9cf1ce939a94d4542f75186e274429d4ba
developerekene <developerekene@gmail.com>
2024-01-21 15:34:07 +0000

    static home page finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import BestServices from "./components/BestServices";
+import BetterExpere from "./components/BetterExpere";
 import FeaturedPost from "./components/FeaturedPost";
 import FeaturedProducts from "./components/FeaturedProducts";
+import Footer from "./components/Footer";
 import Furniture from "./components/Furniture";
 import NavBar from "./components/NavBar";
 import TopNavBar from "./components/TopNavBar";
+import WhatTheySay from "./components/WhatTheySay";
 
 export default function Home() {
   return (
@@ -14,6 +17,9 @@ export default function Home() {
       <FeaturedProducts />
       <BestServices />
       <FeaturedPost />
+      <WhatTheySay />
+      <BetterExpere />
+      <Footer />
     </>
   );
 }
